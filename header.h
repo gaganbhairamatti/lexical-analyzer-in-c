9a68c5cc4c963e5c40ca97c7a05bff49433bebf7
gaganbhairamatti <gaganbhairamatti7@gmail.com>
2026-05-09 11:08:59 +0530

    Add files via upload

---
+++ header.h
@@ -0,0 +1,22 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+
+#define RED     "\033[1;31m"
+#define GREEN   "\033[1;32m"
+#define YELLOW  "\033[1;33m"
+#define BLUE    "\033[1;34m"
+#define RESET   "\033[0m"
+
+
+/* Keyword checker */
+int check_keyword(const char *word);
+
+/* Operator checker */
+int check_operator(char ch);
+
+/* Symbol checker */
+int check_symbol(char ch);
+
+#endif
